7d5ddf1f9d3a9b8f534abcfcf139385cf6f0b2cf
yoheikikuta <diracdiego@gmail.com>
2019-12-10 15:29:30 +0900

    [Add] Add boot.s with putting one character
    First character written by me in the display!

---
+++ prog/src/03_boot_putc/boot.s
@@ -0,0 +1,48 @@
+    BOOT_LOAD  equ  0x7C00
+    ORG  BOOT_LOAD
+;-----------------------
+; Entry point
+;-----------------------
+entry:
+	jmp    ipl
+
+    ;-----------------------
+    ; BIOS Parameter Block (BPB)
+    ;-----------------------
+    times  90 - ($ - $$) db 0x90
+
+    ;-----------------------
+    ; Initial Program Loader (IPL)
+    ;-----------------------
+ipl:
+    cli                    ;Forbid interruption
+
+    mov    ax, 0x0000
+    mov    ds, ax
+    mov    es, ax
+    mov    ss, ax
+    mov    sp, BOOT_LOAD
+
+    sti                    ;Allow interruption
+
+    mov    [BOOT.DRIVE], dl
+
+    mov    al, 'A'
+    mov    ah, 0x0E
+    mov    bx, 0x0000
+    int    0x10
+
+    ;-----------------------
+    ; Finish procedure
+    ;-----------------------
+    jmp    $
+
+ALIGN 2, db 0
+BOOT:
+.DRIVE:     dw 0
+
+;-----------------------
+; Boot flag
+;-----------------------
+    times  510 - ($ - $$) db 0x00
+    db 0x55, 0xAA
