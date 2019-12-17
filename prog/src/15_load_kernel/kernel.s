9252b53d010dbe76e00e2af97419f48f1fff6338
yoheikikuta <diracdiego@gmail.com>
2019-12-17 23:01:30 +0900

    [Add] boot.s and kernel.s
    Just to load kernel which does nothing.
    Also define functions to translate LBA -> CHS.
    
    [Add] Forgot to commit

---
+++ prog/src/15_load_kernel/kernel.s
@@ -0,0 +1,23 @@
+;-----------------------
+; Macro
+;-----------------------
+%include  "../include/define.s"
+%include  "../include/macro.s"
+
+    ORG  BOOT_LOAD     ; load address of kernel
+
+[BITS 32]
+;-----------------------
+; Entry point
+;-----------------------
+kernel:
+    ;-----------------------
+    ; Finish procedure
+    ;-----------------------
+    jmp    $               ; while (1)
+
+
+;-----------------------
+; Padding
+;-----------------------
+    times KERNEL_SIZE - ($ - $$) db 0  ; padding
--- prog/src/include/define.s
+++ prog/src/include/define.s
@@ -1,7 +1,11 @@
     BOOT_LOAD  equ  0x7C00  ; Load position of boot program
-
     BOOT_SIZE  equ  (1024 * 8)  ; Size of booot code
+    BOOT_END   equ  (BOOT_LOAD + BOOT_SIZE)
     SECT_SIZE  equ  (512)  ; Size of sector
     BOOT_SECT  equ  (BOOT_SIZE / SECT_SIZE)  ; Sector number of boot program
 
     E820_RECORD_SIZE  equ  20
+
+    KERNEL_LOAD  equ  0x0010_1000
+    KERNEL_SIZE  equ  (1024 * 8)  ; kernel size
+    KERNEL_SECT  equ  (KERNEL_SIZE / SECT_SIZE)
