1a61c599f5550c2a1738e0192c6dfd163ddf93ae
yoheikikuta <diracdiego@gmail.com>
2019-12-17 23:08:17 +0900

    Change definition order for visiblity

--- prog/src/include/define.s
+++ prog/src/include/define.s
@@ -1,7 +1,8 @@
+    SECT_SIZE  equ  (512)  ; Size of sector
+
     BOOT_LOAD  equ  0x7C00  ; Load position of boot program
     BOOT_SIZE  equ  (1024 * 8)  ; Size of booot code
     BOOT_END   equ  (BOOT_LOAD + BOOT_SIZE)
-    SECT_SIZE  equ  (512)  ; Size of sector
     BOOT_SECT  equ  (BOOT_SIZE / SECT_SIZE)  ; Sector number of boot program
 
     E820_RECORD_SIZE  equ  20
